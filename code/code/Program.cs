137fa5264cd2315ad7874e6158cfa44d308ccd5f
HinataUp <19965331661@163.com>
2023-03-22 10:30:49 +0800

    update 基础知识

--- code/code/Program.cs
+++ code/code/Program.cs
@@ -1,21 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Security.Cryptography;
+
 namespace test;
 
 class Program {
-    // 定义一个委托，用于处理字符串
-    delegate string StringHandler(string str);
-
     static void Main() {
-        string[] arr = { "hello", "world", "csharp" };
-
-        // 创建一个委托实例，指向ToUpper方法
-        StringHandler handler = new StringHandler(str => str.ToUpper());
-
-        // 使用委托处理字符串数组
-        foreach (string str in arr) {
-            string result = handler(str);
-            Console.WriteLine(result);
+        for (int j = 0; j < 3; j++) {
+            for(int i = 0; i < 5; i++){
+                if(i == 4){
+                    break ;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("j = " + j);
         }
+        
     }
 }
