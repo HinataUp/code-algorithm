774b4bd9dab3c4fb9a477c74b358e68a3817aed8
HinataUp <19965331661@163.com>
2023-03-23 21:43:11 +0800

    c# 面相对象 核心知识点

--- code/code/Program.cs
+++ code/code/Program.cs
@@ -5,16 +5,34 @@ using System.Security.Cryptography;
 namespace test;
 
 class Program {
-    static void Main() {
-        for (int j = 0; j < 3; j++) {
-            for(int i = 0; i < 5; i++){
-                if(i == 4){
-                    break ;
-                }
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("j = " + j);
-        }
-        
+    public static void Main() {
+        p kiki = new p("kiki", 11, null) {
+        };
+        p mac = new p("mac", 10, kiki) {
+        };
+        mac.name = "mac";
+        Console.WriteLine(mac.friend.name + mac.friend.age);
+        return;
+    }
+}
+
+class p {
+    public string name;
+    public int age;
+    public p friend;
+
+    public string Name {
+        get;
+        set;
+    }
+
+    public p(string name, int age, p friend) {
+        this.name = name;
+        this.age = age;
+        this.friend = friend;
+    }
+
+    ~p() {
+        Console.WriteLine("bye");
     }
 }
