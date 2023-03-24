e2f3c7bc1ac8abec316f26f8be5ac903d77ae1b8
HinataUp <19965331661@163.com>
2023-03-24 17:03:16 +0800

    update

--- code/code/Program.cs
+++ code/code/Program.cs
@@ -1,30 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections;
 using System.Text;
 
 namespace code;
 
-class Test {
-    public int a = 1;
-    public Test2 b = new Test2();
-
-    // 保护类型 包裹一下
-    public Test Clone() {
-        // return (test)base.MemberwiseClone();
-        return MemberwiseClone() as Test;
-    }
-}
-
-class Test2 {
-    public int a2 = 2;
-}
-
 internal static class My {
     public static void Main() {
-        StringBuilder sb = new StringBuilder("hello world");
-        sb.AppendFormat(" {0}{1}","hello","world");
-        
-        Console.WriteLine(sb.ToString());
+        MyOther<MyClass,IMyInterface> myOther = new MyOther<MyClass,IMyInterface>();
         return;
+    }
+}
+class MyOther<T,U> where T : U {
+    public T value;
+    public void MyTest<K,V>(K k) where K : V {
+        // do something
+    }
+} 
+// 接口
+interface IMyInterface {
+    void MyMethod();
+}
+// 实现
+class MyClass : IMyInterface {
+    public void MyMethod() {
+        // do something
     }
 }
